13f66d3d2bf1049d000a08db2cef9b1215fbda37
nikitair <stlrvnk@gmail.com>
2025-04-12 19:58:58 +0300

    sending data to Retool

--- src/contact_us/routers.py
+++ src/contact_us/routers.py
@@ -1,21 +1,10 @@
-import os
-
-from dotenv import load_dotenv
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request, status
 
 from contact_us.schemas import ContactUsRequest, ContactUsResponse
+from contact_us.utils import ContactUsUtils
 from logging_config import logger
 from schemas import LocationIP
 from utils import Utils
-
-load_dotenv(override=True)
-
-RETOOL_WORKFLOW_URL = os.getenv("RETOOL_WORKFLOW_URL")
-RETOOL_WORKFLOW_API_KEY = os.getenv("RETOOL_WORKFLOW_API_KEY")
-
-if not RETOOL_WORKFLOW_URL or not RETOOL_WORKFLOW_API_KEY:
-    raise ValueError("No Workflow API Key or URL")
-
 
 router: APIRouter = APIRouter(prefix="/contact_us", tags=["contact_us"])
 
@@ -30,6 +19,15 @@ async def submit_contact_us(form_data: ContactUsRequest, request: Request):
     location: LocationIP = Utils.define_location_by_ip(ip)
     logger.info(f"Location by IP - {location}")
 
-    return ContactUsResponse(
+    payload: ContactUsResponse = ContactUsResponse(
         **form_data.model_dump(), ip=ip, city=location.city, country=location.country
     )
+
+    sent_to_retool: bool | None = ContactUsUtils.send_contact_us_to_retool(payload)
+    if sent_to_retool is not True:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Contact Us Form processing failed on the Retool side",
+        )
+
+    return payload
---
+++ src/contact_us/utils.py
@@ -0,0 +1,48 @@
+import os
+
+import httpx
+from dotenv import load_dotenv
+from httpx import HTTPStatusError, RequestError, Response
+
+from contact_us.schemas import ContactUsResponse
+from logging_config import logger
+
+load_dotenv(override=True)
+
+RETOOL_WORKFLOW_URL = os.getenv("RETOOL_WORKFLOW_URL")
+RETOOL_WORKFLOW_API_KEY = os.getenv("RETOOL_WORKFLOW_API_KEY")
+
+if not RETOOL_WORKFLOW_URL or not RETOOL_WORKFLOW_API_KEY:
+    raise ValueError("No Workflow API Key or URL")
+
+
+class ContactUsUtils:
+    @staticmethod
+    def send_contact_us_to_retool(payload: ContactUsResponse) -> bool | None:
+        logger.info(
+            f"sending Contact Us dorm enriched payload to Retool - {payload.model_dump_json()}"
+        )
+
+        try:
+            response: Response = httpx.post(
+                url=RETOOL_WORKFLOW_URL,
+                headers={"X-Workflow-Api-Key": RETOOL_WORKFLOW_API_KEY},
+                json=payload.model_dump(),
+                timeout=10,
+            )
+            logger.info(
+                f"status code - {response.status_code}; response text - {response.text}"
+            )
+            response.raise_for_status()
+        except HTTPStatusError as e:
+            logger.error(
+                f"HTTP error occurred: {e.response.status_code} - {e.response.text}"
+            )
+        except RequestError as e:
+            logger.error(f"Request error occurred: {e}")
+        except Exception as e:
+            logger.error(
+                f"Unexpected error occurred while sending Contact Us for to Retool - {payload.model_dump_json()}; error - {e}"
+            )
+        else:
+            return True
