27067794d9d71cc5346180e20a79b37de1244ae7
nikitair <stlrvnk@gmail.com>
2025-04-22 22:18:18 +0300

    refactor: "" -> ''

--- src/contact_us/routers.py
+++ src/contact_us/routers.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter
 from contact_us.schemas import ContactUsRequest, ContactUsResponse
 from contact_us.services import ContactUsService
 
-router: APIRouter = APIRouter(prefix="/contact_us", tags=["contact_us"])
+router: APIRouter = APIRouter(prefix='/contact_us', tags=['contact_us'])
 
 
-@router.post("/", response_model=ContactUsResponse)
+@router.post('/', response_model=ContactUsResponse)
 async def submit_contact_us(form_data: ContactUsRequest):
     return ContactUsService.send_contact_us_to_retool(form_data)
--- src/logging_config.py
+++ src/logging_config.py
@@ -5,17 +5,17 @@ import logfire
 
 from .config import config
 
-logger: Logger = logging.getLogger("backend")
+logger: Logger = logging.getLogger('backend')
 logger.setLevel(logging.DEBUG)
 logfire.configure(
     token=config.LOGFIRE_TOKEN,
-    service_name="backend",
+    service_name='backend',
     send_to_logfire=True,
-    console=logfire.ConsoleOptions(min_log_level="debug"),
+    console=logfire.ConsoleOptions(min_log_level='debug'),
     distributed_tracing=False,
 )
 
-logger.addHandler(logfire.LogfireLoggingHandler(level="DEBUG"))
+logger.addHandler(logfire.LogfireLoggingHandler(level='DEBUG'))
 
-if __name__ == "__main__":
-    logger.info("Hello there!")
+if __name__ == '__main__':
+    logger.info('Hello there!')
