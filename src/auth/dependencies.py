20e917e3a6a3dfced6442dd4fc35e65c00689baf
nikitair <stlrvnk@gmail.com>
2025-04-29 20:57:18 +0300

    add test endpoint to test auth

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -11,16 +11,18 @@ from all_models import User
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/token')
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_session)) -> User:
-    decoded_token = await AuthUtils.decode_access_token(token)
-    if decoded_token is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
+class AuthDependencies:
+    @staticmethod
+    async def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_session)) -> User:
+        decoded_token = await AuthUtils.decode_access_token(token)
+        if decoded_token is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
 
-    email: str | None = decoded_token.get('sub')
-    if email is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
+        email: str | None = decoded_token.get('sub')
+        if email is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
 
-    user: User | None = session.exec(select(User).where(User.email == email)).one_or_none()
-    if user is None or user.is_disabled:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
-    return user
+        user: User | None = session.exec(select(User).where(User.email == email)).one_or_none()
+        if user is None or user.is_disabled:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
+        return user
--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session
-from typing import Any
 
 from all_models import User
 from auth.schemas import TokenRequest, Token
 from auth.services import AuthService
 from database import get_session
+
+from auth.dependencies import AuthDependencies
 
 router: APIRouter = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -18,3 +19,8 @@ async def get_auth_token(payload: TokenRequest, session: Session = Depends(get_s
 @router.post('/decodeToken')
 async def decode_token(token: str) -> dict | None:
     return await AuthService.decode_token(token)
+
+
+@router.get('/test')
+async def test_auth(current_user: User = Depends(AuthDependencies.get_current_user)) -> dict:
+    return {'email': current_user.email, 'username': current_user.username}
