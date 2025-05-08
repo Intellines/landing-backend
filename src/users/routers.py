3f03d7120dae6b265303245ab79e392e7c236840
sakhaline <alexandra.ivva@gmail.com>
2025-05-08 21:30:27 +0300

    update user router paths

--- src/users/routers.py
+++ src/users/routers.py
@@ -14,7 +14,7 @@ async def get_all_users(session: Session = Depends(get_session)) -> list[User]:
     return await UserService.get_all_users(session)
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED)
+@router.post('', status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate, session: Session = Depends(get_session)) -> User:
     return await UserService.create_user(user, session)
 
@@ -29,7 +29,7 @@ async def get_user_by_username(username: str, session: Session = Depends(get_ses
     return await UserService.get_user_by_username(username, session)
 
 
-@router.delete('/')
+@router.delete('')
 async def delete_user(user_id: int, session: Session = Depends(get_session)) -> User:
     return await UserService.delete_user(user_id, session)
 
